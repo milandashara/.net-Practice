b6eb0800700a802a51fc087e6808ba69a5d93723
milandashara <milandashara@gmail.com>
2014-05-07 18:14:48 +0800

    test

---
+++ MovieRental/Areas/Admin/Controllers/MovieController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieRental.Models;
+using MovieRental.Data;
+using System.Data.Entity.Validation;
+using MovieRental.Externals;
+
+
+namespace MovieRental.Areas.Admin.Controllers
+{
+    public class MovieController : Controller
+    {
+        MovieRepository rep = new MovieRepository();
+
+
+        //
+        // GET: /Admin/Movie/
+
+        public ActionResult Index()
+        {
+            IEnumerable<Movie> movies = rep.GetAllMovies();
+            return View(movies);
+        }
+
+        //
+        // GET: /Admin/Movie/Details/5
+
+        public ActionResult Details(string id)
+        {
+            Movie movie = rep.getMovie(id);
+            return View(movie);
+        }
+
+        //
+        // GET: /Admin/Movie/Create
+
+        public ActionResult Create()
+        {
+            Movie m = new Movie();
+            return View(m);
+        }
+
+        //
+        // POST: /Admin/Movie/Create
+
+        [HttpPost]
+        public ActionResult Create(Movie movie)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                rep.Add(movie);
+                rep.Save();
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                if (e.GetType() != typeof(DbEntityValidationException))
+                {
+                    if (this.HttpContext.IsDebuggingEnabled)
+                    {
+                        ModelState.AddModelError(string.Empty, e.ToString());
+
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "tech error");
+                    }
+
+                    
+                }
+                return View(movie);
+            }
+        }
+
+        //
+        // GET: /Admin/Movie/Edit/5
+
+        public ActionResult Edit(string id)
+        {
+            Movie movie = rep.getMovie(id);
+            return View(movie);
+        }
+
+        //
+        // POST: /Admin/Movie/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(string id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                Movie m = rep.getMovie(id);
+                UpdateModel(m);
+                rep.Save();
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                if (e.GetType() != typeof(DbEntityValidationException))
+                {
+                    if (this.HttpContext.IsDebuggingEnabled)
+                    {
+                        ModelState.AddModelError(string.Empty, e.ToString());
+
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "tech error");
+                    }
+
+
+                }
+                return View();
+
+            }
+        }
+
+        //
+        // GET: /Admin/Movie/Delete/5
+
+        public ActionResult Delete(string id)
+        {
+
+
+            return View(rep.getMovie(id));
+        }
+
+        //
+        // POST: /Admin/Movie/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(string id, Movie movie)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                rep.Delete(id);
+                rep.Save();
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                if (e.GetType() != typeof(DbEntityValidationException))
+                {
+                    if (this.HttpContext.IsDebuggingEnabled)
+                    {
+                        ModelState.AddModelError(string.Empty, e.ToString());
+
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "tech error");
+                    }
+
+
+                }
+                return View(movie);
+
+            }
+        }
+
+        [HttpPost]
+        public ActionResult OMDBSearch(string title)
+        {
+            Movie m = OMDBService.GetMoviesByTitle(title);
+            return Json(m);
+        }
+    }
+}
---
+++ MovieRental/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MovieRental")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("MovieRental")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4f2c2878-e80d-45aa-bad7-a6e02a7c8c7d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
